48c2e1ad2e3045da830d159e79af60f7b781a9f6
Luis Mendoza <lemt.09@gmail.com>
2023-06-23 07:13:40 -0500

    Saludo Luis Mendoza

--- HolaMundo.java
+++ HolaMundo.java
@@ -3,7 +3,9 @@ import static java.lang.System.out;
 public class HolaMundo {
 
 	public static void main(String[] args) {
+
 		NicolasFiquitivaSegura.Saludo();
+		LuisMendoza.Saludo();
 	}
 
 }
---
+++ LuisMendoza.java
@@ -0,0 +1,7 @@
+import static java.lang.System.out;
+
+    public class LuisMendoza {
+        public static void Saludo() {
+            out.println("Luis Mendoza dice:     Primero que nada y antes que todo, buenos dias 🤙");
+        }
+}
