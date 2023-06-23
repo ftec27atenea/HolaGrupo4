b8bd9404f1a47530541d691f4caf0b227d138430
Nicolas Fiquitiva Segura <Nico52105@gmail.com>
2023-06-23 06:49:37 -0500

    Agregamos programa de saludo

---
+++ NicolasFiquitivaSegura.java
@@ -0,0 +1,8 @@
+import static java.lang.System.out;
+public class NicolasFiquitivaSegura {
+    public static void Saludo() {
+        out.println("Nicolas Fiquitiva Segura dice:        Buen día grupo 4");
+    }
+}
+
+
